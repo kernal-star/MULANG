f69738c950af4fb13533bfbbbace60735ca33e38
Chaitanya biswas <chaitanya.biswas.22@gmail.com>
2022-12-07 22:07:03 +0530

    done bro

--- src/components/ChannelContainer.jsx
+++ src/components/ChannelContainer.jsx
@@ -1,8 +1,11 @@
-import React from "react";
-const ChannelContainer =()=>{
-    return(
-        <div>
-             ChannelContainer
-            </div>
-    );
+import React from 'react'
+
+const ChannelContainer = () => {
+  return (
+    <div>
+       <u>what your dog doing</u>
+    </div>
+  )
 }
+
+export default ChannelContainer
--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -1,27 +1,11 @@
-import React from "react";
-import { logChatPromiseExecution } from "stream-chat";
-import { ChannelList, useChatContext } from "stream-chat-react";
-import Cookies from "universal-cookie";
-import logosvg from '..assests/mulang.png';
-const sodeBar=()=>(
-    <div className="channel-List_sidebar">
-    <div className="channel-List_sidebar_icon1">
-    
-    <div className="icon1__inner">
-      <img src={logosvg} alt="Mulang" width="30">
-      </img>
-    
-    </div>
-    </div>
-    </div>
-)
+import React from 'react'
 
-
-const ChannelListContainer =()=>{
-    return(
-        <div>
-
-            ChannelListContainer
-            </div>
-    );
+const ChannelListContainer = () => {
+  return (
+    <div>
+        <p>hello world</p>
+    </div>
+  )
 }
+
+export default ChannelListContainer
