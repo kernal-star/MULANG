53b6daeb4ee8b7365f4d6ecff7bad6678dd16a45
Chaitanya biswas <chaitanya.biswas.22@gmail.com>
2022-12-07 12:48:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { StreamChat } from 'stream-chat';
+import { ChannelList, Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer,ChannelContainer } from "./components";
+const apiKey ='4ynken6zwh3u';
+const client = StreamChat.getInstance(apiKey);
+const App =()=>{
+    return(
+        <div className="app__wrapper">
+
+            <Chat client={client} theme="team light">
+                <ChannelListContainer>
+
+                </ChannelListContainer>
+            </Chat>
+            <h1>chap application</h1>
+            </div>
+    );
+}
