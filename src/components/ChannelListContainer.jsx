b3de9b77a380c0fb5a16a4d43caf00a88d61fe3c
Chaitanya biswas <chaitanya.biswas.22@gmail.com>
2022-12-11 18:34:30 +0530

    channel,eh

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
-
+import { ChannelList, useChatContext } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+const SideBar = () =>(
+  <div className='channel-List__sidebar'>
+    
+    </div>
+)
 const ChannelListContainer = () => {
   return (
     <div>
